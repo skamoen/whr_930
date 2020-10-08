d3397f5062ada527095aed6a463bf9b3d32ce96d
Sille Kamoen <github@sille.me>
2020-10-08 13:11:13 +0200

    Debug checksum fails

--- src/whr930.py
+++ src/whr930.py
@@ -161,9 +161,12 @@ def validate_data(data, data_raw):
     else:
         if len(data) >= 10:
             # Validate checksum
-            checksum = raw_data_checksum(data_raw)
-            if (checksum != int(data_raw[-3], 16)):
-                debug_msg("Checksum doesn't match")
+            try:
+                checksum = raw_data_checksum(data_raw)
+                if (checksum != int(data_raw[-3], 16)):
+                    debug_msg("Checksum doesn't match")
+            except ValueError as err:
+                debug_msg("Can't calculate checksum over {} \n full data: {}".format(data[2,-3], data))
             """
             A valid response should be at least 10 bytes (ACK + response with data length = 0)
 
